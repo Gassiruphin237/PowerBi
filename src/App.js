4fabd32d643a053fdcfa7cfb9a862dca758215d4
Gassiruphin237 <gassiruphin@gmail.com>
2024-06-03 20:06:42 +0200

    projet 1.0 update 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link, Navigate } from 'react-router-dom';
 import Login from './components/Pages/Login';
 import Forgot from './components/Pages/Forgot';
 
@@ -7,6 +7,7 @@ function App() {
     return (
         <div className="App">
             <Routes>
+               <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/forgot-password" element={<Forgot/>} />
             </Routes>
