3ad9a163ce1f644b1b4fc3a97d08d0eb8397081c
Gassiruphin237 <gassiruphin@gmail.com>
2024-08-05 15:31:30 +0100

    save

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Forgot from './components/Pages/Forgot';
 import Otp from './components/Pages/Otp';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import NewPassword from './components/Pages/NewPassword';
+import Home from './components/Pages/Home';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/forgot-password" element={<Forgot/>} />
                 <Route path="/otp" element={<Otp />} />
                 <Route path="/NewPassword" element={<NewPassword />} />
---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "../../styles/Home.css";
+function Home() {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='bloc1 '>
+                    <div className='head'>
+                        <p className='title'>Solde</p>
+                        <p className='amount'>XAF 20000</p>
+                    </div>
+                    <div className='icon'>
+
+
+                    </div>
+
+                </div>
+                <div className='bloc1'>
+
+
+                </div>
+                <div className='bloc1'>
+
+
+                </div>
+                <div className='bloc1'>
+
+
+                </div>
+            </div>
+            {/* <div className='graph'>
+                <div className='conatain'>
+
+
+                </div>
+            </div> */}
+        </div>
+    )
+}
+
+export default Home
